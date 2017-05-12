9ae3bc49b1b352e9683db05140a5af808b3668b5
momoroso2013 <keitamomotani@Keita-no-MacBook-Air.local>
2017-05-12 16:56:00 +0900

    edit機能を追加する

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,7 +19,13 @@ class GroupsController < ApplicationController
   end
 
   def edit
-     # @group = Group.find(params[:id])
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    group = Group.find(params[:id])
+    group.update(group_params)
+    redirect_to :root, notice: "グループを編集しました"
   end
 
   private
