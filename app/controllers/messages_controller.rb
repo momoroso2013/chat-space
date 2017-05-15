51cc3b62a1d778e321ee41ceefaa77adf9dc200c
momoroso2013 <keitamomotani@Keita-no-MacBook-Air.local>
2017-05-15 12:18:05 +0900

    fix message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,16 +2,13 @@ class MessagesController < ApplicationController
 
 before_action :group_find
 
-
 def index
   @message = Message.new
   @groups = current_user.groups.order("id DESC")
-  group_find
 end
 
 def create
   @message = current_user.messages.new(message_params)
-  group_find
   if @message.save
     redirect_to group_messages_path, notice: "メッセージを送信しました。"
   else
@@ -23,12 +20,11 @@ end
 private
 
 def group_find
-    @group = Group.find(params[:group_id])
-
+  @group = Group.find(params[:group_id])
 end
 
 def message_params
-  params.require(:message).permit(:text).merge(group_id: params[:group_id], user_id: current_user.id)
+  params.require(:message).permit(:text).merge(group_id: params[:group_id])
 end
 
 end
