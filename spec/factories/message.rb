72279e490a13aae9ecf47017db50f9ce2f79c6ec
momoroso2013 <keitamomotani@Keita-no-MacBook-Air.local>
2017-05-15 18:02:55 +0900

    テストの実行

---
+++ spec/factories/message.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+
+  factory :message do
+    text              "テキストテキストテキストテキスト"
+    image                 "イメージイメージ"
+    group_id              "10"
+    user_id               "2"
+  end
+end
