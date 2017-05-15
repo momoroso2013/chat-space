e2cf2b82343c0e9821bc7ed9c6b1a99fb942c147
momoroso2013 <keitamomotani@Keita-no-MacBook-Air.local>
2017-05-15 11:15:52 +0900

    create view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,20 @@
 class MessagesController < ApplicationController
 
 def index
+  @message = Message.new
   @groups = current_user.groups.order("id DESC")
   @group = Group.find(params[:group_id])
+end
+
+def create
+  Message.create(text: message_params[:text], image:message_params[:image], group_id: message_params[:group_id],user_id: message_params[:user_id])
+  redirect_to group_messages_path
+end
+
+private
+
+def message_params
+  params.require(:message).permit(:text).merge(group_id: params[:group_id], user_id: current_user.id)
 end
 
 end
