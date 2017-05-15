a0db9e0cd28efb2aa607b8136bf86f15eb47b9a5
momoroso2013 <keitamomotani@Keita-no-MacBook-Air.local>
2017-05-15 12:00:33 +0900

    create flash message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,31 @@
 class MessagesController < ApplicationController
 
+before_action :group_find
+
+
 def index
   @message = Message.new
   @groups = current_user.groups.order("id DESC")
-  @group = Group.find(params[:group_id])
+  group_find
 end
 
 def create
-  Message.create(text: message_params[:text], image:message_params[:image], group_id: message_params[:group_id],user_id: message_params[:user_id])
-  redirect_to group_messages_path
+  @message = current_user.messages.new(message_params)
+  group_find
+  if @message.save
+    redirect_to group_messages_path, notice: "メッセージを送信しました。"
+  else
+    flash.now[:alert] = "メッセージを入力してください。"
+    render :index
+  end
 end
 
 private
+
+def group_find
+    @group = Group.find(params[:group_id])
+
+end
 
 def message_params
   params.require(:message).permit(:text).merge(group_id: params[:group_id], user_id: current_user.id)
