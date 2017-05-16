624684b95ea5eecf5d5f74681ca3cadfda5bb8be
momoroso2013 <keitamomotani@Keita-no-MacBook-Air.local>
2017-05-16 20:34:53 +0900

    fix indent

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -11,30 +11,30 @@ describe MessagesController do
         get :index, group_id: group.id
       end
 
-    it "renders the :index template" do
-      expect(response).to render_template :index
-    end
+      it "renders the :index template" do
+        expect(response).to render_template :index
+      end
 
-    it "assigns the requested message to @message" do
-      expect(assigns(:message)).to be_a_new(Message)
-    end
+      it "assigns the requested message to @message" do
+        expect(assigns(:message)).to be_a_new(Message)
+      end
 
-    it "populates an array of messages ordered by created_at DESC" do
-      expect(assigns(:messages)).to match(messages)
-    end
+      it "populates an array of messages ordered by created_at DESC" do
+        expect(assigns(:messages)).to match(messages)
+      end
 
-    it "assigns the requested contact to @groups" do
-      group = user.groups.first
-      groups = user.groups
-      expect(assigns(:groups)).to eq groups
-    end
+      it "assigns the requested contact to @groups" do
+        group = user.groups.first
+        groups = user.groups
+        expect(assigns(:groups)).to eq groups
+      end
     end
 
     context "when user has NOT signed in" do
-    it "redirect_to new_user_session_path" do
-      get :index, group_id: group.id
-      expect(response).to redirect_to (new_user_session_path)
-    end
+      it "redirect_to new_user_session_path" do
+        get :index, group_id: group.id
+        expect(response).to redirect_to (new_user_session_path)
+      end
     end
   end
 end
