7ba089bbe6b6fea5534169d72115876db3dab805
momoroso2013 <keitamomotani@Keita-no-MacBook-Air.local>
2017-05-24 15:15:59 +0900

    add search action to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+  def search
+    @users = User.where("name LIKE(?)", "%#{user_params[:name]}%")
+    respond_to do |format|
+      format.json
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
